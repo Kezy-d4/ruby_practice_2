5207180669b5ad5318170d8f070f76264f2be3b5
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-06-23 17:21:26 -0400

    exercise: write missing_letter method

--- c_s_g_t_d_s_a/hash_table_exercises.rb
+++ c_s_g_t_d_s_a/hash_table_exercises.rb
@@ -7,4 +7,14 @@ def first_duplicate(arr)
   end
 end
 
-p first_duplicate(%w[a b d d c c e f]) # => "d"
+# p first_duplicate(%w[a b d d c c e f]) # => "d"
+
+def missing_letter(str)
+  index = {}
+  alphabet = "a".."z"
+  str.each_char { |char| index[char] = true }
+  alphabet.each { |ltr| return ltr unless index[ltr] }
+end
+
+# str = "the quick brown box jumps over a lazy dog"
+# p missing_letter(str)
